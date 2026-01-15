ff86054acaf8865f454e175405bce22bd9bced28
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-01-15 13:43:43 -0500

    Delete mail_config.py

--- mail_config.py
+++
@@ -1,7 +0,0 @@
-MAIL_SETTINGS = {
-    "smtp_server": "smtp.gmail.com",
-    "smtp_port": 587,
-    "sender_email": "magicbankia@gmail.com",
-    "app_password": "vorkwpkfnxyahgwm",
-    "receiver_email": "magicbankia@gmail.com"
-}
