b5d515ba8eb1ca5f27671d41fbccc5a44009dde8
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-01-15 13:42:29 -0500

    Create mail_config.py

---
+++ notifications/mail_config.py
@@ -0,0 +1,7 @@
+MAIL_SETTINGS = {
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587,
+    "sender_email": "magicbankia@gmail.com",
+    "app_password": "vorkwpkfnxyahgwm",
+    "receiver_email": "magicbankia@gmail.com"
+}
