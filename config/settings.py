4bc0b5f0e00856814063340eaba0357dcf58fa5b
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-01-15 13:40:05 -0500

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+# ==============================
+# MagicBank IA AutoSentry
+# Configuración general
+# ==============================
+
+MAIL_SETTINGS = {
+    "sender_email": "magicbankia@gmail.com",
+    "receiver_email": "magicbankia@gmail.com"
+}
+
+SYSTEM_SETTINGS = {
+    "project_name": "MagicBank IA AutoSentry",
+    "environment": "production",
+    "auto_mode": True
+}
