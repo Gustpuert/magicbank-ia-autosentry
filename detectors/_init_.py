636e0d0ab2631ca878cd2271e6e5540445dd9735
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-01-16 17:54:34 -0500

    Update _init_.py

--- detectors/_init_.py
+++ detectors/_init_.py
@@ -1,1 +1,5 @@
-
+from .derecho import DerechoDetector
+from .tributario import TributarioDetector
+from .administracion import AdministracionDetector
+from .marketing import MarketingDetector
+from .software import SoftwareDetector
