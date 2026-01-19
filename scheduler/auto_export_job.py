b85eeebc9fc4d8ad62ee01762f60a96fe5ed2dc1
Gustavo Puerta Ramirez <gustavopuerta@yahoo.com>
2026-01-19 11:28:43 -0500

    Update auto_export_job.py

--- scheduler/auto_export_job.py
+++ scheduler/auto_export_job.py
@@ -1,5 +1,7 @@
 from export.auto_exporter import AutoHistoryExporter
 from export.uploader import HistoryUploader
+from export.mailer import HistoryMailer
+import os
 
 
 def run():
@@ -7,14 +9,25 @@ def run():
     result = exporter.run()
 
     uploader = HistoryUploader()
-    publish_result = uploader.upload(
+    published = uploader.upload(
         pdf_path=result["pdf"],
         signature_path=result["signature"]
     )
 
-    print("[OK] Historial generado y publicado")
-    print(f"PDF: {publish_result['pdf']}")
-    print(f"Firma: {publish_result['signature']}")
+    mailer = HistoryMailer(
+        smtp_server="smtp.gmail.com",
+        smtp_port=465,
+        email_sender=os.getenv("MAIL_SENDER"),
+        email_password=os.getenv("MAIL_PASSWORD"),
+        email_receiver=os.getenv("MAIL_RECEIVER")
+    )
+
+    mailer.send(
+        pdf_path=published["pdf"],
+        signature_path=published["signature"]
+    )
+
+    print("[OK] PDF generado, firmado, publicado y enviado por correo.")
 
 
 if __name__ == "__main__":
